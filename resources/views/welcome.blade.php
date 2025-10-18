fda40960ac0df3b1f09359f6511f5d83355355e4
SharpLync <jannie.marinda@gmail.com>
2025-10-18 17:19:53 +1000

    Auto deploy on Sat-18-10_1719

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,7 +108,7 @@
         text-align:center;
         z-index:3;
     ">
-      <a href="{{ url('/home') }}"
+      <a href="{{ url('/style-preview') }}"
          style="
             display:inline-block;
             border:2px solid #ffffff;
