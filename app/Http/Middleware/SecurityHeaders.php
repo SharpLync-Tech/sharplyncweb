eb6c5613a9bdadedb9a6bf96547e4fed70689e70
SharpLync <jannie.marinda@gmail.com>
2025-10-27 11:49:20 +1000

    Adjust CSP to allow Azure and self-hosted assets

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -14,9 +14,9 @@ class SecurityHeaders
         // --- Security Headers ---
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
         $response->headers->set(
-            'Content-Security-Policy',
-            "default-src 'self' https://sharplync.com.au https://sharplink.com.au; img-src 'self' data: https:; style-src 'self' 'unsafe-inline'; script-src 'self';"
-        );
+                        'Content-Security-Policy',
+                        "default-src 'self' https://sharplync.com.au https://sharplink.com.au https://*.azurewebsites.net; img-src 'self' data: https:; style-src 'self' 'unsafe-inline'; font-src 'self' data:; script-src 'self' 'unsafe-inline' https:;"
+                    );
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
