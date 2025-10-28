67b4fcdb5e0bef31b68d0b4b8e38b88626796639
SharpLync <jannie.marinda@gmail.com>
2025-10-28 11:59:50 +1000

    Auto deploy on Tue-28-10_1159

--- resources/views/test-logo.blade.php
+++ resources/views/test-logo.blade.php
@@ -3,10 +3,12 @@
 @section('title', 'SharpLync | Test Logo')
 
 @section('content')
-<div class="tiles-wrapper">
-    <div class="tile">Tile 1</div>
-    <div class="tile">Tile 2</div>
-    <div class="tile">Tile 3</div>
-    <div class="tile">Tile 4</div>
+<div class="tiles-container">
+    <div class="tiles-wrapper">
+        <div class="tile">Tile 1</div>
+        <div class="tile">Tile 2</div>
+        <div class="tile">Tile 3</div>
+        <div class="tile">Tile 4</div>
+    </div>
 </div>
 @endsection
