65302829d3fdac301709a5b51627fd2bb5cd454c
SharpLync <jannie.marinda@gmail.com>
2025-10-28 10:52:28 +1000

    Auto deploy on Tue-28-10_1052

--- resources/views/test-logo.blade.php
+++ resources/views/test-logo.blade.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SharpLync | Test Logo</title>
+@extends('layouts.app')
+
+@section('title', 'SharpLync Test Logo')
+
+@section('content')
+
   <style>
     body {
       margin: 0;
@@ -46,3 +46,4 @@
   </div>
 </body>
 </html>
+@endsection
