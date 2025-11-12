f0e1c4f92b1c7b68fc033792adbd0a6dd442f9b8
SharpLync <jannie.marinda@gmail.com>
2025-11-12 19:14:49 +1000

    Auto deploy on Wed-12-11_1914

--- resources/views/customers/layouts/customer-layout-test.blade.php
+++ resources/views/customers/layouts/customer-layout-test.blade.php
@@ -11,7 +11,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title') | SharpLync Portal</title>
-  <link rel="stylesheet" href="/css/customer.css">
+  <link rel="stylesheet" href="/css/customer_test.css">
 </head>
 <body class="cp-root">
   <header class="cp-header">
