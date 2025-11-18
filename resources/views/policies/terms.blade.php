1180209a2f39c81a98040c5ef61901c8855c363b
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-19 09:47:06 +1000

    Auto deploy on Wed-19-11_947

--- resources/views/policies/terms.blade.php
+++ resources/views/policies/terms.blade.php
@@ -1,5 +1,4 @@
 {{-- resources/views/policies/terms.blade.php --}}
-
 @extends('layouts.policy-base')
 
 @section('title', 'SharpLync | Terms & Conditions')
@@ -9,14 +8,31 @@
 @endsection
 
 {{-- These values will be passed from the controller later --}}
-@section('pdf-url', $pdfUrl ?? null) 
+@section('pdf-url', $pdfUrl ?? null)
 
 @section('policy-content')
-    {{-- This variable is where your converted PDF text will go. --}}
-    {{-- We use {!! !!} to render HTML if the content is marked up (e.g., from PDF conversion) --}}
-    @if(isset($content) && !empty($content))
-        {!! $content !!}
-    @else
-        <p>The Terms & Conditions document is currently unavailable. Please check back later.</p>
-    @endif
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 col-xl-8">
+                <div class="card shadow-lg">
+                    <div class="card-body p-4">
+                        {{-- This variable is where your converted PDF text will go. --}}
+                        {{-- We use {!! !!} to render HTML if the content is marked up (e.g., from PDF conversion) --}}
+                        @if(isset($content) && !empty($content))
+                            {!! $content !!}
+                        @else
+                            <p>The Terms & Conditions document is currently unavailable. Please check back later.</p>
+                        @endif
+
+                        {{-- Download link at the bottom --}}
+                        @if(isset($pdfUrl) && !empty($pdfUrl))
+                            <div class="text-center mt-4">
+                                <a href="{{ $pdfUrl }}" class="btn btn-primary" download>Download the official document (PDF)</a>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
