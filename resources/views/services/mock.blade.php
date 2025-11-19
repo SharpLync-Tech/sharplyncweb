5bfd3cfd38930869c5231ab44161bc2e56f364fc
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-19 15:31:29 +1000

    Auto deploy on Wed-19-11_1531

--- resources/views/services/mock.blade.php
+++ resources/views/services/mock.blade.php
@@ -10,28 +10,33 @@
 @section('content')
 <section class="hero services-hero">
 
+    {{-- CPU background --}}
     <div class="hero-cpu-bg">
         <img src="{{ asset('images/hero-cpu.png') }}" alt="SharpLync CPU Background">
     </div>
 
+    {{-- Logo --}}
     <img src="{{ asset('images/sharplync-logo.png') }}"
          alt="SharpLync Hero Logo"
          class="hero-logo">
 
+    {{-- Hero Heading --}}
     <div class="hero-text">
         <h1>Your Business,<br>
             <span class="highlight">Secure & Connected.</span>
         </h1>
     </div>
 
+    {{-- Service Tiles --}}
     <div class="hero-cards fade-section services-cards">
 
         @foreach ($categories as $cat)
         <div class="tile service-tile" data-id="{{ $cat['id'] }}">
 
+            {{-- TILE HEADER --}}
             <div class="tile-header">
                 @if (!empty($cat['icon']))
-                <img src="{{ asset($cat['icon']) }}"
+                <img src="{{ asset($cat['icon']) }}" 
                      alt="{{ $cat['title'] }} Icon"
                      class="tile-icon">
                 @endif
@@ -39,49 +44,55 @@
                 <h3>{{ $cat['title'] }}</h3>
                 <p>{{ $cat['short'] }}</p>
 
-                <button type="button" class="tile-toggle">Learn More</button>
+                <button type="button" class="tile-toggle">
+                    Learn More
+                </button>
             </div>
 
             {{-- EXPANDED DETAIL --}}
             <div class="tile-detail">
 
+                {{-- DIVIDER LINE --}}
+                <hr class="tile-divider">
+
                 @if (!empty($cat['image']))
                 <div class="tile-detail-inner">
 
-                    {{-- LEFT IMAGE --}}
+                    {{-- IMAGE LEFT --}}
                     <div class="detail-image-wrapper">
                         <img src="{{ asset($cat['image']) }}"
                              alt="{{ $cat['title'] }} Image"
                              class="detail-image">
                     </div>
 
-                    {{-- RIGHT CONTENT --}}
+                    {{-- TEXT RIGHT --}}
                     <div class="tile-detail-content">
+
                         <p>{{ $cat['long'] }}</p>
 
                         @if (!empty($cat['subs']))
-                        <h4>Included Services</h4>
-                        <ul>
-                            @foreach ($cat['subs'] as $sub)
-                            <li>{{ $sub }}</li>
-                            @endforeach
-                        </ul>
+                            <h4>Included Services</h4>
+                            <ul>
+                                @foreach ($cat['subs'] as $sub)
+                                    <li>{{ $sub }}</li>
+                                @endforeach
+                            </ul>
                         @endif
+
                     </div>
 
                 </div>
 
-                {{-- FALLBACK: NO IMAGE --}}
+                {{-- NO IMAGE → CLASSIC CENTERED TEXT --}}
                 @else
                     <p>{{ $cat['long'] }}</p>
-
                     @if (!empty($cat['subs']))
-                    <h4>Included Services</h4>
-                    <ul>
-                        @foreach ($cat['subs'] as $sub)
-                        <li>{{ $sub }}</li>
-                        @endforeach
-                    </ul>
+                        <h4>Included Services</h4>
+                        <ul>
+                            @foreach ($cat['subs'] as $sub)
+                                <li>{{ $sub }}</li>
+                            @endforeach
+                        </ul>
                     @endif
                 @endif
 
