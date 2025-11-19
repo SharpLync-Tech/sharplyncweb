9616e76ea104557abb431e67d67edf23a3f79b26
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-19 12:55:08 +1000

    Auto deploy on Wed-19-11_1255

--- resources/views/services/mock.blade.php
+++ resources/views/services/mock.blade.php
@@ -9,57 +9,85 @@
 
 @section('content')
 <section class="hero services-hero">
-    {{-- CPU image, same as home --}}
+
+    {{-- CPU Background --}}
     <div class="hero-cpu-bg">
         <img src="{{ asset('images/hero-cpu.png') }}" alt="SharpLync CPU Background">
     </div>
 
-    {{-- Center logo, same as home --}}
+    {{-- Hero Logo --}}
     <img src="{{ asset('images/sharplync-logo.png') }}"
-         alt="SharpLync Hero Logo"
+         alt="SharpLync Logo"
          class="hero-logo">
 
-    {{-- Hero text, services-specific --}}
+    {{-- Hero Text --}}
     <div class="hero-text">
         <h1>Your Business,<br>
             <span class="highlight">Secure &amp; Connected.</span>
-        </h1>        
+        </h1>
     </div>
 
-    {{-- Service tiles: same style as home (.tile), but with extra behaviour --}}
+    {{-- SERVICE TILES --}}
     <div class="hero-cards fade-section services-cards">
+
         @foreach ($categories as $cat)
             <div class="tile service-tile" data-id="{{ $cat['id'] }}">
-                <div class="tile-header">
-                    @if (!empty($cat['icon']))
-                        <img src="{{ asset($cat['icon']) }}"
-                             alt="{{ $cat['title'] }}"
-                             class="tile-icon">
-                    @endif
 
-                    <h3>{{ $cat['title'] }}</h3>
-                    <p>{{ $cat['short'] }}</p>
+                {{-- NEW EXPANDED LAYOUT WRAPPER --}}
+                <div class="tile-expanded-layout">
 
-                    <button type="button" class="tile-toggle">
-                        Learn More
-                    </button>
-                </div>
+                    {{-- LEFT SIDE IMAGE --}}
+                    <div class="tile-media">
+                        @if (!empty($cat['image']))
+                            <img src="{{ asset($cat['image']) }}" alt="{{ $cat['title'] }}">
+                        @else
+                            {{-- Optional fallback --}}
+                            <img src="{{ asset('images/default-service.jpg') }}" alt="{{ $cat['title'] }}">
+                        @endif
+                    </div>
 
-                <div class="tile-detail">
-                    <p>{{ $cat['long'] }}</p>
+                    {{-- RIGHT CONTENT BLOCK --}}
+                    <div class="tile-content">
 
-                    @if (!empty($cat['subs']))
-                        <h4>Included Services</h4>
-                        <ul>
-                            @foreach ($cat['subs'] as $sub)
-                                <li>{{ $sub }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                </div>
+                        {{-- HEADER / ICON / SHORT TEXT --}}
+                        <div class="tile-header">
+                            @if (!empty($cat['icon']))
+                                <img src="{{ asset($cat['icon']) }}"
+                                     alt="{{ $cat['title'] }}"
+                                     class="tile-icon">
+                            @endif
+
+                            <h3>{{ $cat['title'] }}</h3>
+                            <p class="tile-short">{{ $cat['short'] }}</p>
+                        </div>
+
+                        {{-- BUTTON --}}
+                        <button type="button" class="tile-toggle">
+                            Learn More
+                        </button>
+
+                        {{-- DETAILS / DESCRIPTION --}}
+                        <div class="tile-detail">
+                            <p>{{ $cat['long'] }}</p>
+
+                            @if (!empty($cat['subs']))
+                                <h4>Included Services</h4>
+                                <ul>
+                                    @foreach ($cat['subs'] as $sub)
+                                        <li>{{ $sub }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </div>
+
+                    </div>{{-- end tile-content --}}
+                </div>{{-- end tile-expanded-layout --}}
+
             </div>
         @endforeach
+
     </div>
+
 </section>
 @endsection
 
