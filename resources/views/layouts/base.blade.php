ec636fee95d34917a6f36fee76378bf00c40fa2e
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-20 10:57:57 +1000

    Auto deploy on Thu-20-11_1057

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -54,11 +54,13 @@
         <button class="close-menu" onclick="toggleMenu()" aria-label="Close navigation menu">×</button>
         <ul>
             <li><a href="/">Home</a></li>
+            <li><a href="/services" onclick="toggleMenu()">Contact Us</a></li>
             <li><a href="/login" onclick="toggleMenu()">Login</a></li>
             <li><a href="/register" onclick="toggleMenu()">Register</a></li>
             <li><a href="/about" onclick="toggleMenu()">About Us</a></li>
             <li><a href="/testimonials" onclick="toggleMenu()">Testimonials</a></li>
-            <li><a href="#contact" onclick="toggleMenu()">Contact Us</a></li>
+            <li><a href="/contact" onclick="toggleMenu()">Contact Us</a></li>
+        
         </ul>
     </div>
 
