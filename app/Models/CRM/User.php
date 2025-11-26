0c3d7c45fc5efda36233546c6a4b8dded583e0d9
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-26 18:55:08 +1000

    Auto deploy on Wed-26-11_1855

--- app/Models/CRM/User.php
+++ app/Models/CRM/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
+        'profile_photo',
         'email',
         'password',
         'account_status',
