feaa27eeb11620476385d9ef11b02b44fb4b55c3
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-26 15:05:21 +1000

    Auto deploy on Wed-26-11_1505

--- resources/views/customers/edit-profile.blade.php
+++ resources/views/customers/edit-profile.blade.php
@@ -1,7 +1,8 @@
 
 
 @section('title', 'Edit Profile')
-<link rel="stylesheet" href="/css/customer.css?v=3003">
+<link rel="stylesheet" href="/css/customer-profile-edit.css">
+<link rel="stylesheet" href="/css/google-places.css">
 @push('styles')
     <link rel="stylesheet" href="{{ secure_asset('css/customer-profile-edit.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/google-places.css') }}">
