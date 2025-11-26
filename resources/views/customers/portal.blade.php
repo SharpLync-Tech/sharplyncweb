7113c9e3489bde62a920035d606cc7a20cb29970
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-27 08:22:28 +1000

    Auto deploy on Thu-27-11_822

--- resources/views/customers/portal.blade.php
+++ resources/views/customers/portal.blade.php
@@ -81,7 +81,7 @@
             <h4>Support</h4>
             <p>Need help? View support tickets or connect for remote assistance.</p>
             <div class="cp-support-footer">
-                <a href="{{ route('customer.support') }}" class="cp-btn cp-small-btn cp-teal-btn">Open Support</a>
+                <a href="{{ route('customer.support.index') }}" class="cp-btn cp-small-btn cp-teal-btn">Open Support</a>
                 <a href="{{ URL::temporarySignedRoute('customer.teamviewer.download', now()->addMinutes(5)) }}"
                    class="cp-btn cp-small-btn cp-teal-btn">
                     Download Quick Support
