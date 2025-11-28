4227a90ff11ab0c106190fa79e57916fc97a94ff
Jannie Brits <jannie.brits@sharplync.com.au>
2025-11-28 14:11:27 +1000

    Auto deploy on Fri-28-11_1411

--- resources/views/support-admin/tickets/partials/message.blade.php
+++ resources/views/support-admin/tickets/partials/message.blade.php
@@ -6,8 +6,32 @@
         </span>
     </div>
 
-    <div class="admin-message-body">
-    {!! $body !!}
+    <div class="{{ $isCustomer ? 'admin-message-customer' : 'admin-message-staff' }}">
+    <div class="admin-message-header">
+        <span class="admin-message-author">{{ $authorName }}</span>
+        <span class="admin-message-time">
+            {{ optional($timestamp)->timezone('Australia/Brisbane')->format('d M Y, H:i') }}
+        </span>
     </div>
+
+    <div class="admin-message-body">
+        {!! $body !!}
+
+        {{-- Attachment (if exists) --}}
+        @if(!empty($attachment_path ?? null))
+            <div class="admin-message-attachment" style="margin-top:10px;">
+                📎 <a href="{{ route('support-admin.attachment.download', $message_id) }}"
+                      style="color:#0A4A7A; font-weight:600;"
+                      target="_blank">
+                    {{ $attachment_original_name }}
+                </a>
+                <div style="font-size:12px; opacity:.7;">
+                    ({{ $attachment_mime }})
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
+
 
 </div>
