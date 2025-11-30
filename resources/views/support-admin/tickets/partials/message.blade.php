fd25abd8b14a4d3debb3c963165e80da526f8f6d
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-01 08:25:56 +1000

    Auto deploy on Mon-01-12_825

--- resources/views/support-admin/tickets/partials/message.blade.php
+++ resources/views/support-admin/tickets/partials/message.blade.php
@@ -4,7 +4,15 @@
         <span class="admin-message-time">
             {{ optional($timestamp)->timezone('Australia/Brisbane')->format('d M Y, H:i') }}
         </span>
-    </div>    
+    </div>
+
+    <div class="{{ $isCustomer ? 'admin-message-customer' : 'admin-message-staff' }}">
+    <div class="admin-message-header">
+        <span class="admin-message-author">{{ $authorName }}</span>
+        <span class="admin-message-time">
+            {{ optional($timestamp)->timezone('Australia/Brisbane')->format('d M Y, H:i') }}
+        </span>
+    </div>
 
     <div class="admin-message-body">
         {!! $body !!}
