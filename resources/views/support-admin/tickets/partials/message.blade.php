aa34629c9cf04a71ca770d66c91c3672884894ed
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-01 10:08:14 +1000

    Auto deploy on Mon-01-12_1008

--- resources/views/support-admin/tickets/partials/message.blade.php
+++ resources/views/support-admin/tickets/partials/message.blade.php
@@ -1,4 +1,6 @@
 <div class="{{ $isCustomer ? 'admin-message-customer' : 'admin-message-staff' }}">
+    
+    {{-- Header --}}
     <div class="admin-message-header">
         <span class="admin-message-author">{{ $authorName }}</span>
         <span class="admin-message-time">
@@ -6,18 +8,11 @@
         </span>
     </div>
 
-    <div class="{{ $isCustomer ? 'admin-message-customer' : 'admin-message-staff' }}">
-    <div class="admin-message-header">
-        <span class="admin-message-author">{{ $authorName }}</span>
-        <span class="admin-message-time">
-            {{ optional($timestamp)->timezone('Australia/Brisbane')->format('d M Y, H:i') }}
-        </span>
-    </div>
-
+    {{-- Body --}}
     <div class="admin-message-body">
         {!! $body !!}
 
-        {{-- Attachment (if exists) --}}
+        {{-- Attachment --}}
         @if(!empty($attachment_path ?? null))
             <div class="admin-message-attachment" style="margin-top:10px;">
                 📎 <a href="{{ route('support-admin.attachment.download', $message_id) }}"
@@ -25,13 +20,12 @@
                       target="_blank">
                     {{ $attachment_original_name }}
                 </a>
+
                 <div style="font-size:12px; opacity:.7;">
                     ({{ $attachment_mime }})
                 </div>
             </div>
         @endif
     </div>
-</div>
-
 
 </div>
