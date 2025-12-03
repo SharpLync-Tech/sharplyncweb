e215050e1b2c2182defce8c295c8ed3ffd3442f7
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-03 20:09:16 +1000

    Auto deploy on Wed-03-12_2009

--- resources/views/layouts/testimonials-base.blade.php
+++ resources/views/layouts/testimonials-base.blade.php
@@ -67,8 +67,9 @@
     </main>
 
     <!-- FOOTER (kept your smaller, clean one) -->
-    <footer class="cp-footer">
-        <p>&copy; {{ date('Y') }} SharpLync Pty Ltd. All rights reserved.</p>
+     <footer>
+        <div class="footer-content">
+            <p>&copy; {{ date('Y') }} SharpLync Pty Ltd. All rights reserved.</p>
             <div class="social-icons">
                 <a href="https://www.linkedin.com/company/sharplync"><img src="{{ asset('images/linkedin.png') }}" alt="LinkedIn"></a>
                 <a href="https://www.facebook.com/SharpLync"><img src="{{ asset('images/facebook.png') }}" alt="Facebook"></a>
