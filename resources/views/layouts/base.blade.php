ce865104cfca7cbbe6d86d91c564ec7081a10ea1
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-03 19:21:44 +1000

    Auto deploy on Wed-03-12_1921

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -87,6 +87,7 @@
             <li><a href="/about">About</a></li>
             <li><a href="/testimonials">Testimonials</a></li>
             <li><a href="/contact">Contact</a></li>
+            <li><a href="/register">Register</a></li>
             <li><a href="/login">Login</a></li>
         </ul>
     </div>
