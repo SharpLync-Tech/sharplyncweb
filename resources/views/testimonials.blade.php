8bf758ff42b22635ad218d4c094b8c36789a0157
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-03 22:08:39 +1000

    Auto deploy on Wed-03-12_2208

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -12,7 +12,9 @@
     <div class="container">
         {{-- Page heading --}}
         <div class="testimonials-header">
-            <h1>The Foundation of SharpLync:<span class="gradient">Trusted Expertise</span></h1>
+            <h1>
+                The Foundation of SharpLync: <span class="gradient">Trusted Expertise</span>
+            </h1>
             <p>Endorsements from industry leaders, colleagues, and partners who vouch for our founder's track record, technical integrity, and commitment to exceptional service.</p>
         </div>
 
