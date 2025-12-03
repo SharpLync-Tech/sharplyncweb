b351173030f79734c93c933d0441f4ae5b31f350
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-03 11:41:30 +1000

    Auto deploy on Wed-03-12_1141

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -1,5 +1,10 @@
+@extends('layouts.base')
 
-@extends('layouts.testimonials-base')
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/testimonials.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+@endpush
 
 @section('title', 'SharpLync | Testimonials')
 
