91027c512c6986a2aa4a928c892781408b0f99b9
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-11 08:22:51 +1000

    Auto deploy on Thu-11-12_822

--- routes/azureai.php
+++ routes/azureai.php
@@ -3,7 +3,7 @@ use App\Services\AzureOpenAIClient;
 
 Route::get('/ai-test', function (AzureOpenAIClient $client) {
 
-    $result = $client->analyze("You have won $5000! Click the link to claim: http://fake-site.scam");
+    $result = $client->analyze("This message says I won a prize and should click a link to claim. Is it a scam?");
 
     return response()->json($result);
 });
