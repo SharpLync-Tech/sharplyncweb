c28e8638416b9eb2dc985423e1eb52576e0b95b9
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-11 10:35:40 +1000

    Auto deploy on Thu-11-12_1035

--- routes/azureai.php
+++ routes/azureai.php
@@ -6,3 +6,11 @@ use App\Http\Controllers\ScamCheckerController;
 // Scam Checker - basic page + analysis handler
 Route::get('/scam-checker', [ScamCheckerController::class, 'index']);
 Route::post('/scam-checker', [ScamCheckerController::class, 'analyze']);
+
+
+Route::get('/ai/debug-model', function () {
+    return [
+        'endpoint' => env('AZURE_OPENAI_ENDPOINT'),
+        'deployment' => env('AZURE_OPENAI_DEPLOYMENT'),
+    ];
+});
