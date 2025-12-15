2430ea2b66cb4154d2387e2994223a5f08907d15
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-15 17:45:18 +1000

    Auto deploy on Mon-15-12_1745

--- resources/views/scamcheck/index.blade.php
+++ resources/views/scamcheck/index.blade.php
@@ -49,14 +49,14 @@
                 @csrf
 
                 <textarea
-                    id="scam-text"
-                    name="message"
-                    rows="10"
-                    placeholder="1. Copy and Paste your email text
-                2. Save and upload an email
-                3. Drag & Drop your saved email file
-                4. Copy and paste you TXT or WhatsApp message text"
-                >{{ $input ?? '' }}</textarea>
+    id="scam-text"
+    name="message"
+    rows="10"
+    placeholder="1. Copy and Paste your email text
+2. Save and upload an email
+3. Drag & Drop your saved email file
+4. Copy and paste you TXT or WhatsApp message text"
+>{{ $input ?? '' }}</textarea>
 
                 <div id="attached-file" class="attached-file" style="display:none;">
                     <span class="file-icon">🛡️ File ready:</span>
