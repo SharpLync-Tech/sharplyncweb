5e7832337144e4cd1f40815a09c8f119b8693242
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-27 12:26:20 +1000

    Auto deploy on Sat-27-12_1226

--- resources/views/layouts/sharpfleet.blade.php
+++ resources/views/layouts/sharpfleet.blade.php
@@ -9,7 +9,7 @@
     <!-- SharpFleet CSS -->
     <link rel="stylesheet" href="{{ asset('css/sharpfleet/sharpfleetmain.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     @stack('styles')
 </head>
 <body>
