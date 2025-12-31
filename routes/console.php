ea6b8ac2ef3fbfeb240f2766ee609fd729263005
Jannie Brits <jannie.brits@sharplync.com.au>
2025-12-31 19:31:28 +1000

    Auto deploy on Wed-31-12_1931

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('sharpfleet:ping')->everyFiveMinutes();
+app()->booted(function () {
+    app(Schedule::class)
+        ->command('sharpfleet:ping')
+        ->everyFiveMinutes();
+});
