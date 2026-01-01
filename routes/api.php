674e8ceb0f55931adf8774656184be2853c3c5b8
Jannie Brits <jannie.brits@sharplync.com.au>
2026-01-01 21:37:12 +1000

    Auto deploy on Thu-01-01_2137

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Admin\Api\DeviceAuditApiController;
 use App\Http\Controllers\SharpFleet\StripeWebhookController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/device-audit', [DeviceAuditApiController::class, 'store']);
 
-Route::post('/sharpfleet/stripe/webhook', [StripeWebhookController::class, 'handle']);
+
+Route::post('/app/sharpfleet/stripe/webhook', [StripeWebhookController::class, 'handle']);
