d15d6eb6218de2c43871797cb335bc042e795d4f
Jannie Brits <jannie.brits@sharplync.com.au>
2026-01-08 08:35:11 +1000

    Auto deploy on Thu-08-01_835

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,13 +31,18 @@ return Application::configure(basePath: dirname(__DIR__))
          */
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
 
+        // 🔐 If you're using cookie-based auth for web SPA clients, enable this:
+        // $middleware->prepend(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
+
         /**
          * ------------------------------------------------------------
          * Route middleware aliases
          * ------------------------------------------------------------
          */
         $middleware->alias([
-            'admin.auth' => \App\Http\Middleware\AdminAuth::class,
+            'admin.auth'    => \App\Http\Middleware\AdminAuth::class,
+            'auth'          => \Illuminate\Auth\Middleware\Authenticate::class,
+            'auth:sanctum'  => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
     })
     ->withProviders([
