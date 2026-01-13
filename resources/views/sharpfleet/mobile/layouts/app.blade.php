fecfd1ee4cc167eb6be8a810742efa88af53d1f1
Jannie Brits <jannie.brits@sharplync.com.au>
2026-01-13 15:25:10 +1000

    Auto deploy on Tue-13-01_1525

--- resources/views/sharpfleet/mobile/layouts/app.blade.php
+++ resources/views/sharpfleet/mobile/layouts/app.blade.php
@@ -11,10 +11,11 @@
 
     {{-- Mobile-only CSS --}}
     <link rel="stylesheet" href="{{ asset('css/sharpfleet/sharpfleet-mobile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/sharpfleet/sharpfleet-sheets.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('css/sharpfleet/sharpfleet-sheets.css') }}">   
     @stack('styles')
 </head>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <body class="sf-mobile">
 
     {{-- App Shell --}}
--- resources/views/sharpfleet/mobile/partials/footer.blade.php
+++ resources/views/sharpfleet/mobile/partials/footer.blade.php
@@ -1,21 +1,66 @@
+@php
+    $path = request()->path();
+
+    $nav = [
+        [
+            'key' => 'home',
+            'label' => 'Home',
+            'icon' => 'home-outline',
+            'url' => '/app/sharpfleet/mobile',
+        ],
+        [
+            'key' => 'start',
+            'label' => 'Start',
+            'icon' => 'play-circle-outline',
+            'url' => '#', // will open Start Trip sheet later
+        ],
+        [
+            'key' => 'history',
+            'label' => 'History',
+            'icon' => 'time-outline',
+            'url' => '/app/sharpfleet/mobile/history',
+        ],
+        [
+            'key' => 'more',
+            'label' => 'More',
+            'icon' => 'menu-outline',
+            'url' => '/app/sharpfleet/mobile/more',
+        ],
+    ];
+@endphp
+
 <nav class="sf-mobile-footer">
-    <a href="#" class="sf-mobile-nav-item active">
-        <span class="icon">🏠</span>
-        <span class="label">Home</span>
-    </a>
+    <div class="sf-mobile-footer-blur">
+        <div class="sf-mobile-footer-tint"></div>
 
-    <a href="#" class="sf-mobile-nav-item">
-        <span class="icon">▶️</span>
-        <span class="label">Start</span>
-    </a>
+        <div class="sf-mobile-footer-inner">
+            @foreach ($nav as $item)
+                @php
+                    $active = str_starts_with($path, ltrim(parse_url($item['url'], PHP_URL_PATH), '/'));
+                @endphp
 
-    <a href="#" class="sf-mobile-nav-item">
-        <span class="icon">🕘</span>
-        <span class="label">History</span>
-    </a>
+                <a
+                    href="{{ $item['url'] }}"
+                    class="sf-mobile-footer-item {{ $active ? 'active' : '' }}"
+                >
+                    <span class="sf-footer-icon-wrap">
+                        @if($active)
+                            <span class="sf-footer-glow">
+                                <ion-icon name="{{ $item['icon'] }}"></ion-icon>
+                            </span>
+                        @endif
 
-    <a href="#" class="sf-mobile-nav-item">
-        <span class="icon">☰</span>
-        <span class="label">More</span>
-    </a>
+                        <ion-icon
+                            name="{{ $item['icon'] }}"
+                            class="sf-footer-icon"
+                        ></ion-icon>
+                    </span>
+
+                    <span class="sf-footer-label">
+                        {{ $item['label'] }}
+                    </span>
+                </a>
+            @endforeach
+        </div>
+    </div>
 </nav>
