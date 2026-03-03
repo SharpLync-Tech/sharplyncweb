ab2ca831e5d58d0bd187fe51728c14b3873ec348
Jannie Brits <jannie.brits@sharplync.com.au>
2026-03-03 12:15:20 +1000

    SharpLync auto deploy on Tue-03-03_1215

--- app/Services/Marketing/MarketingHtmlFormatter.php
+++ app/Services/Marketing/MarketingHtmlFormatter.php
@@ -27,6 +27,27 @@ class MarketingHtmlFormatter
         // Collapse excessive line breaks.
         $html = preg_replace('/(<br\s*\/?>\s*){3,}/i', '<br><br>', $html);
 
+        // Tighten common signature blocks starting with "Regards".
+        $html = preg_replace_callback(
+            '/<p([^>]*)>\s*Regards,?\s*<\/p>\s*<p([^>]*)>(.*?)<\/p>\s*<p([^>]*)>(.*?)<\/p>/is',
+            function ($matches) {
+                $applyStyle = function (string $attrs, string $style): string {
+                    if (stripos($attrs, 'style=') !== false) {
+                        return preg_replace('/style=(["\'])(.*?)\\1/i', 'style="$2; ' . $style . '"', $attrs, 1);
+                    }
+                    return ' style="' . $style . '"' . $attrs;
+                };
+
+                $p1 = '<p' . $applyStyle($matches[1], 'margin:0 0 6px 0; line-height:1.4;') . '>Regards,</p>';
+                $p2 = '<p' . $applyStyle($matches[2], 'margin:0 0 6px 0; line-height:1.4;') . '>' . $matches[3] . '</p>';
+                $p3 = '<p' . $applyStyle($matches[4], 'margin:0; line-height:1.4;') . '>' . $matches[5] . '</p>';
+
+                return $p1 . $p2 . $p3;
+            },
+            $html,
+            1
+        );
+
         return trim($html);
     }
 }
